bc5101155096d1f4941f93d0b003ae90f833b900
= <=>
2022-07-05 20:37:48 +0300

    my first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const port = 3001;
+
+const usersController = require('./controllers/users-controller');
+const vacationsController = require('./controllers/vacations-controller');
+const likedVacationsController = require('./controllers/likedVacations-controller');
+const loginFilter = require('./filters/login-filter');
+const server = express();
+
+server.use(cors({ origin: 'http://localhost:3000' }));
+// server.use(loginFilter());
+server.use(express.json());
+server.use('/users', usersController);
+server.use('/vacations', vacationsController);
+server.use('/likedVacations', likedVacationsController);
+
+server.listen(3001, () => console.log(`Listening on http://localhost:${port}`));
